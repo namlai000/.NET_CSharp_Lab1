ac79e121dbf286ae52bb775655853bc2a45f2b02
namlai000 <theguncrusher@gmail.com>
2016-07-15 18:04:00 +0700

    Catch errors [FINAL VERSION]

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -25,133 +25,140 @@ namespace Lab1
 
             while (loop)
             {
-                Console.WriteLine("1. Add new Product");
-                Console.WriteLine("2. Update product");
-                Console.WriteLine("3. Delete product");
-                Console.WriteLine("4. Search product by name");
-                Console.WriteLine("5. Search product by price range");
-                Console.WriteLine("6: Find products belong to a manufacturer");
-                Console.WriteLine("7: Display all products");
-                Console.WriteLine("8: Exit");
-                Console.Write("Enter a number: ");
-                int menu = int.Parse(Console.ReadLine());
-                switch (menu)
+                try
                 {
-                    case 1:
-                        #region Add
-                        Console.Write("Enter code: ");
-                        code = Console.ReadLine();
-                        exist = manager.findByCode(code);
-                        if (exist)
-                        {
-                            Console.WriteLine("Product exist! Try agian please");
-                        }
-                        else
-                        {
+                    Console.WriteLine("1. Add new Product");
+                    Console.WriteLine("2. Update product");
+                    Console.WriteLine("3. Delete product");
+                    Console.WriteLine("4. Search product by name");
+                    Console.WriteLine("5. Search product by price range");
+                    Console.WriteLine("6: Find products belong to a manufacturer");
+                    Console.WriteLine("7: Display all products");
+                    Console.WriteLine("8: Exit");
+                    Console.Write("Enter a number: ");
+                    int menu = int.Parse(Console.ReadLine());
+                    switch (menu)
+                    {
+                        case 1:
+                            #region Add
+                            Console.Write("Enter code: ");
+                            code = Console.ReadLine();
+                            exist = manager.findByCode(code);
+                            if (exist)
+                            {
+                                Console.WriteLine("Product exist! Try agian please");
+                            }
+                            else
+                            {
+                                Console.Write("Enter name: ");
+                                name = Console.ReadLine();
+                                Console.Write("Enter price: ");
+                                price = double.Parse(Console.ReadLine());
+                                Console.Write("Enter quantity: ");
+                                quantity = double.Parse(Console.ReadLine());
+                                Console.Write("Enter manufacturer: ");
+                                manufacturer = Console.ReadLine();
+
+                                p = new Product(code, name, price, quantity, manufacturer);
+                                manager.addProduct(p);
+                            }
+                            break;
+                            #endregion
+                        case 2:
+                            #region Update
+                            Console.Write("Enter code: ");
+                            code = Console.ReadLine();
+                            exist = manager.findByCode(code);
+                            if (!exist)
+                            {
+                                Console.WriteLine("Product not found!");
+                            }
+                            else
+                            {
+                                Console.Write("Enter name: ");
+                                name = Console.ReadLine();
+                                Console.Write("Enter price: ");
+                                price = double.Parse(Console.ReadLine());
+                                Console.Write("Enter quantity: ");
+                                quantity = double.Parse(Console.ReadLine());
+                                Console.Write("Enter manufacturer: ");
+                                manufacturer = Console.ReadLine();
+
+                                p = new Product(code, name, price, quantity, manufacturer);
+                                manager.updateProduct(p);
+                            }
+                            #endregion
+                            break;
+                        case 3:
+                            #region Delete
+                            Console.Write("Enter code: ");
+                            code = Console.ReadLine();
+                            exist = manager.findByCode(code);
+                            if (!exist)
+                            {
+                                Console.WriteLine("Product not found!");
+                            }
+                            else
+                            {
+                                p = new Product();
+                                p.code = code;
+                                manager.deleteProduct(p);
+                                Console.WriteLine("Product deleted");
+                            }
+                            #endregion
+                            break;
+                        case 4:
+                            #region search name
                             Console.Write("Enter name: ");
                             name = Console.ReadLine();
-                            Console.Write("Enter price: ");
-                            price = double.Parse(Console.ReadLine());
-                            Console.Write("Enter quantity: ");
-                            quantity = double.Parse(Console.ReadLine());
+                            p = manager.findByName(name);
+                            if (p != null)
+                            {
+                                Console.WriteLine(p.ToString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("Product not found!");
+                            }
+                            #endregion
+                            break;
+                        case 5:
+                            #region search price
+                            Console.Write("Enter lowest price: ");
+                            double r1 = double.Parse(Console.ReadLine());
+                            Console.Write("Enter highest price: ");
+                            double r2 = double.Parse(Console.ReadLine());
+                            p = manager.findByPriceRange(r1, r2);
+                            if (p != null)
+                            {
+                                Console.WriteLine(p.ToString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("Product not found!");
+                            }
+                            #endregion
+                            break;
+                        case 6:
+                            #region Search manufacturer
                             Console.Write("Enter manufacturer: ");
                             manufacturer = Console.ReadLine();
-
-                            p = new Product(code, name, price, quantity, manufacturer);
-                            manager.addProduct(p);
-                        }
-                        break; 
-                        #endregion
-                    case 2:
-                        #region Update
-                        Console.Write("Enter code: ");
-                        code = Console.ReadLine();
-                        exist = manager.findByCode(code);
-                        if (!exist)
-                        {
-                            Console.WriteLine("Product not found!");
-                        }
-                        else
-                        {
-                            Console.Write("Enter name: ");
-                            name = Console.ReadLine();
-                            Console.Write("Enter price: ");
-                            price = double.Parse(Console.ReadLine());
-                            Console.Write("Enter quantity: ");
-                            quantity = double.Parse(Console.ReadLine());
-                            Console.Write("Enter manufacturer: ");
-                            manufacturer = Console.ReadLine();
-
-                            p = new Product(code, name, price, quantity, manufacturer);
-                            manager.updateProduct(p);
-                        }
-                        #endregion 
-                        break;
-                    case 3:
-                        #region Delete
-                        Console.Write("Enter code: ");
-                        code = Console.ReadLine();
-                        exist = manager.findByCode(code);
-                        if (!exist)
-                        {
-                            Console.WriteLine("Product not found!");
-                        }
-                        else
-                        {
-                            p = new Product();
-                            p.code = code;
-                            manager.deleteProduct(p);
-                            Console.WriteLine("Product deleted");
-                        }
-                        #endregion
-                        break;
-                    case 4:
-                        #region search name
-                        Console.Write("Enter name: ");
-                        name = Console.ReadLine();
-                        p = manager.findByName(name);
-                        if (p != null)
-                        {
-                            Console.WriteLine(p.ToString());
-                        }
-                        else
-                        {
-                            Console.WriteLine("Product not found!");
-                        }
-                        #endregion
-                        break;
-                    case 5:
-                        #region search price
-		                Console.Write("Enter lowest price: ");
-                        double r1 = double.Parse(Console.ReadLine());
-                        Console.Write("Enter highest price: ");
-                        double r2 = double.Parse(Console.ReadLine());
-                        p = manager.findByPriceRange(r1, r2);
-                        if (p != null)
-                        {
-                            Console.WriteLine(p.ToString());
-                        }
-                        else
-                        {
-                            Console.WriteLine("Product not found!");
-                        }
-	                    #endregion
-                        break;
-                    case 6:
-                        #region Search manufacturer
-                        Console.Write("Enter manufacturer: ");
-                        manufacturer = Console.ReadLine();
-                        manager.displayByManufacturer(manufacturer);
-                        break; 
-                        #endregion
-                    case 7:
-                        #region display all
-                        manager.viewProducts();
-                        break; 
-                        #endregion
-                    default:
-                        loop = false;
-                        break;
+                            manager.displayByManufacturer(manufacturer);
+                            break;
+                            #endregion
+                        case 7:
+                            #region display all
+                            manager.viewProducts();
+                            break;
+                            #endregion
+                        default:
+                            loop = false;
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
                 }
             }
         }
